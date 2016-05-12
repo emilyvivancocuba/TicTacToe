5a2e047d890608c8f527c91401f5369aaf2c72ce
emilyvivancocuba <emilyvivancocuba@gmail.com>
2016-05-12 15:22:20 -0500

    asignacion de x y o

--- js/main.js
+++ js/main.js
@@ -1,27 +1,16 @@
-$(document).ready(function(){
-	var suma=0;
-	var ganador='';
-	for (var i = 1; i <=9; i++) {
-		if ($('#i').val()==='x'||$('#i').val()==='o') {
-			suma+=1;
+var boton_jugar = document.getElementsByClassName('play');
+var contar=0;
+for (var i=0; i<9; i++) {
+	boton_jugar[i].onclick=function(){
+		contar++;
+		if (contar%2===0) {
+			this.textContent = "x";
+		}else{
+			this.textContent = "o";
 		}
 	}
-	if (suma>=3) {
-		if ($('#1').val()===$('#2').val()&&$('#1').val()===$('#3').val()||
-			$('#1').val()===$('#4').val()&&$('#1').val()===$('#7').val()||
-			$('#1').val()===$('#5').val()&&$('#1').val()===$('#9').val()){
-			ganador=$('#1').val();
-		}
-		if ($('#2').val()===$('#5').val()&&$('#2').val()===$('#8').val()||
-			$('#3').val()===$('#5').val()&&$('#3').val()===$('#7').val()||
-			$('#4').val()===$('#5').val()&&$('#4').val()===$('#6').val()){
-			ganador=$('#5').val();
-		}
-		if ($('#3').val()===$('#6').val()&&$('#3').val()===$('#9').val()||
-			$('#7').val()===$('#8').val()&&$('#7').val()===$('#9').val()) {
-			ganador=$('#9').val();
-		}
-	}
-	return ganador;
-});
-alert('El ganador es'+ganador);
+}
+var reset = document.getElementsByClassName('resetear');
+reset.onclick=function(){
+	boton_jugar
+}
